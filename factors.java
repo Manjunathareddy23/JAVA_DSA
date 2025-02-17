9671f0f00c68147a88718560436139f6e3185674
Kummathi Manjunatha Reddy <141892277+Manjunathareddy23@users.noreply.github.com>
2025-02-17 21:40:50 +0530

    Create factors.java

---
+++ factors.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int n=10;
+        int fact=1;
+        for(int i =1;i<=n;i++){
+            if(n%i==0){
+                    System.out.println(i);
+            }
+        }
+    
+        
+    }
+}
